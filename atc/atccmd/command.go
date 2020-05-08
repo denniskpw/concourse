0bf5dd516ee0679e55fb8e407470d16c33a31d08
Aidan Oldershaw <aoldershaw@pivotal.io>
2020-05-08 13:51:06 -0400

    db: structure: wire an EventStore to DB entities
    The motivation for introducing an EventStore is two-fold:
    
    1. To support external stores for build events to avoid filling the core
       database with build logs. For instance, we could add an ElasticSearch
       EventStore for managing build events, while keeping everything else
       in Postgres
    2. To support arbitrary Event processing before storing to the final
       EventStore. For instance, secret redaction is currently implemented
       by having a modified `dbEventWriterWithSecretRedaction` that
       essentially just delegates to the main `dbEventWriter` after
       redacting any secrets. This could possibly be implemented as an
       `EventStore` following the Decorator pattern that receives full log
       events, redacts secrets from them, and then delegates to another
       EventStore. While the initial approach works, it's difficult to add
       more event processing - the initial motivation for this was #3879.
       Another thing this lets us do is e.g. syslog forwarding in real time,
       rather than having a syslog drainer component.
    
    This takes an alternative approach to my initial attempted refactor that
    can began in this commit: 8eff6c7e0a81e7ac6baed9dbe571be4e3f0c3a40
    
    The main difference is that everything is contained in the `db` package
    here, rather than passing the `EventStore` to every component that could
    perform an action that produces events. Additionally, I chose to use the
    name `EventStore` instead of `EventProcessor`. While I argued that a
    generic name like `EventProcessor` would be beneficial to encourage
    general data transformations, I feel like it's a bit too general - and,
    if we follow the Decorator pattern, every `EventProcessor` will
    necessarily need to be an `EventStore` as well, so might as well just
    call it what it is.
    
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- atc/atccmd/command.go
+++ atc/atccmd/command.go
@@ -587,7 +587,9 @@ func (cmd *RunCommand) constructAPIMembers(
 		return nil, err
 	}
 
-	teamFactory := db.NewTeamFactory(dbConn, lockFactory)
+	eventStore := constructEventStore(dbConn)
+
+	teamFactory := db.NewTeamFactory(dbConn, lockFactory, eventStore)
 
 	_, err = teamFactory.CreateDefaultTeamIfNotExists()
 	if err != nil {
@@ -646,12 +648,12 @@ func (cmd *RunCommand) constructAPIMembers(
 	workerClient := worker.NewClient(pool, workerProvider, compressionLib)
 
 	credsManagers := cmd.CredentialManagers
-	dbPipelineFactory := db.NewPipelineFactory(dbConn, lockFactory)
-	dbJobFactory := db.NewJobFactory(dbConn, lockFactory)
-	dbResourceFactory := db.NewResourceFactory(dbConn, lockFactory)
+	dbPipelineFactory := db.NewPipelineFactory(dbConn, lockFactory, eventStore)
+	dbJobFactory := db.NewJobFactory(dbConn, lockFactory, eventStore)
+	dbResourceFactory := db.NewResourceFactory(dbConn, lockFactory, eventStore)
 	dbContainerRepository := db.NewContainerRepository(dbConn)
 	gcContainerDestroyer := gc.NewDestroyer(logger, dbContainerRepository, dbVolumeRepository)
-	dbBuildFactory := db.NewBuildFactory(dbConn, lockFactory, cmd.GC.OneOffBuildGracePeriod, cmd.GC.FailedGracePeriod)
+	dbBuildFactory := db.NewBuildFactory(dbConn, lockFactory, eventStore, cmd.GC.OneOffBuildGracePeriod, cmd.GC.FailedGracePeriod)
 	dbCheckFactory := db.NewCheckFactory(dbConn, lockFactory, secretManager, cmd.varSourcePool, cmd.GlobalResourceCheckTimeout)
 	dbClock := db.NewClock()
 	dbWall := db.NewWall(dbConn, &dbClock)
@@ -822,7 +824,9 @@ func (cmd *RunCommand) constructBackendMembers(
 		syslogDrainConfigured = false
 	}
 
-	teamFactory := db.NewTeamFactory(dbConn, lockFactory)
+	eventStore := constructEventStore(dbConn)
+
+	teamFactory := db.NewTeamFactory(dbConn, lockFactory, eventStore)
 
 	resourceFactory := resource.NewResourceFactory()
 	dbResourceCacheFactory := db.NewResourceCacheFactory(dbConn, lockFactory)
@@ -896,10 +900,10 @@ func (cmd *RunCommand) constructBackendMembers(
 		lockFactory,
 	)
 
-	dbBuildFactory := db.NewBuildFactory(dbConn, lockFactory, cmd.GC.OneOffBuildGracePeriod, cmd.GC.FailedGracePeriod)
+	dbBuildFactory := db.NewBuildFactory(dbConn, lockFactory, eventStore, cmd.GC.OneOffBuildGracePeriod, cmd.GC.FailedGracePeriod)
 	dbCheckFactory := db.NewCheckFactory(dbConn, lockFactory, secretManager, cmd.varSourcePool, cmd.GlobalResourceCheckTimeout)
-	dbPipelineFactory := db.NewPipelineFactory(dbConn, lockFactory)
-	dbJobFactory := db.NewJobFactory(dbConn, lockFactory)
+	dbPipelineFactory := db.NewPipelineFactory(dbConn, lockFactory, eventStore)
+	dbJobFactory := db.NewJobFactory(dbConn, lockFactory, eventStore)
 
 	componentFactory := db.NewComponentFactory(dbConn)
 
@@ -1041,7 +1045,8 @@ func (cmd *RunCommand) constructGCMember(
 	dbArtifactLifecycle := db.NewArtifactLifecycle(gcConn)
 	dbCheckLifecycle := db.NewCheckLifecycle(gcConn)
 	resourceConfigCheckSessionLifecycle := db.NewResourceConfigCheckSessionLifecycle(gcConn)
-	dbBuildFactory := db.NewBuildFactory(gcConn, lockFactory, cmd.GC.OneOffBuildGracePeriod, cmd.GC.FailedGracePeriod)
+	eventStore := constructEventStore(gcConn)
+	dbBuildFactory := db.NewBuildFactory(gcConn, lockFactory, eventStore, cmd.GC.OneOffBuildGracePeriod, cmd.GC.FailedGracePeriod)
 	dbResourceConfigFactory := db.NewResourceConfigFactory(gcConn, lockFactory)
 
 	dbVolumeRepository := db.NewVolumeRepository(gcConn)
@@ -1077,8 +1082,12 @@ func (cmd *RunCommand) constructGCMember(
 	return members, nil
 }
 
-func (cmd *RunCommand) validateCustomRoles() error {
+func constructEventStore(dbConn db.Conn) db.EventStore {
+	// TODO: actually construct something!
+	return nil
+}
 
+func (cmd *RunCommand) validateCustomRoles() error {
 	path := cmd.ConfigRBAC.Path()
 	if path == "" {
 		return nil
